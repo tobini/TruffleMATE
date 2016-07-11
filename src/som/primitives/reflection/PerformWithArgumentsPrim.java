050a40e41e356b9b3d99d4c94a18160c2df47eed
Guido Chari <charig@gmail.com>
2016-07-11 19:10:20 -0300

    Perform with can receive array of args or just the arg value as in pharo

--- src/som/primitives/reflection/PerformWithArgumentsPrim.java
+++ src/som/primitives/reflection/PerformWithArgumentsPrim.java
@@ -8,6 +8,7 @@ import com.oracle.truffle.api.dsl.GenerateNodeFactory;
 import com.oracle.truffle.api.dsl.Specialization;
 import com.oracle.truffle.api.frame.VirtualFrame;
 import com.oracle.truffle.api.nodes.NodeCost;
+import com.oracle.truffle.api.object.DynamicObject;
 
 
 @GenerateNodeFactory
@@ -24,7 +25,26 @@ public abstract class PerformWithArgumentsPrim extends TernaryExpressionNode {
       final Object receiver, final SSymbol selector, final SArray  argsArr) {
     return dispatch.executeDispatch(frame, receiver, selector, argsArr);
   }
-
+  
+  /*Todo: Remove the conversion to SArray and then conversion to normal array in the dispatch if a performance problem emerges*/
+  @Specialization
+  public final Object doObject(final VirtualFrame frame,
+      final Object receiver, final SSymbol selector, final long arg) {
+    return dispatch.executeDispatch(frame, receiver, selector, SArray.create(new Object[]{arg}));
+  }
+  
+  @Specialization
+  public final Object doObject(final VirtualFrame frame,
+      final Object receiver, final SSymbol selector, final double arg) {
+    return dispatch.executeDispatch(frame, receiver, selector, SArray.create(new Object[]{arg}));
+  }
+  
+  @Specialization
+  public final Object doObject(final VirtualFrame frame,
+      final Object receiver, final SSymbol selector, final DynamicObject arg) {
+    return dispatch.executeDispatch(frame, receiver, selector, SArray.create(new Object[]{arg}));
+  }
+  
   @Override
   public NodeCost getCost() {
     return dispatch.getCost();
