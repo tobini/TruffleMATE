d0036e03f2831eae6b11076ecd881f21615af650
Guido Chari <charig@gmail.com>
2016-08-18 09:47:04 -0300

    Add operation tag and source section to length primitive

--- src/tools/dym/nodes/ArrayAllocationProfilingNode.java
+++ src/tools/dym/nodes/ArrayAllocationProfilingNode.java
@@ -14,7 +14,7 @@ public class ArrayAllocationProfilingNode extends CountingNode<ArrayCreationProf
 
   public ArrayAllocationProfilingNode(final ArrayCreationProfile counter) {
     super(counter);
-    size = LengthPrimFactory.create(null);
+    size = LengthPrimFactory.create(null, null);
   }
 
   @Override
