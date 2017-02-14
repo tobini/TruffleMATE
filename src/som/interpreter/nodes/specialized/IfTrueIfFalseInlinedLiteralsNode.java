d5d0965e601f48fe7151090b32522facf7a7f48d
Guido Chari <charig@gmail.com>
2017-02-14 16:36:15 -0300

    Update core library. Merge som and correct style

--- src/som/interpreter/nodes/specialized/IfTrueIfFalseInlinedLiteralsNode.java
+++ src/som/interpreter/nodes/specialized/IfTrueIfFalseInlinedLiteralsNode.java
@@ -64,10 +64,10 @@ public final class IfTrueIfFalseInlinedLiteralsNode extends ExpressionWithTagsNo
       return falseNode.executeGeneric(frame);
     }
   }
-  
+
   @Override
   public Node asMateNode() {
-    //Must do someting with inlined if and mate!
+    // Must do someting with inlined if and mate!
     return null;
   }
 }
